bc8a6e664897d43141c7005c168c961d61839049
Brian Rinaldi <brian.rinaldi@gmail.com>
2024-08-06 17:13:50 -0400

    Logger changes
    Updated the logger example to show a more complex usage for logging events locally.

--- logger-cdk/lib/logger-stack.ts
+++ logger-cdk/lib/logger-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import {LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 export class LoggerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
    super(scope, id, props);
    const loggerFunction = new NodejsFunction(this, 'loggerFunction', {
       entry: '../logger-lambda/index.ts',
       handler: 'handler',
+      environment: {
+        Powertools_SERVICE_NAME: 'helloWorld',
+        LOG_LEVEL: 'INFO',
+      },
+      logRetention: RetentionDays.ONE_WEEK,
     });
 
     new cdk.CfnOutput(this, 'LoggerFunction', {
--- logger-lambda/index.ts
+++ logger-lambda/index.ts
@@ -1,8 +1,17 @@
 
 import { Logger } from '@aws-lambda-powertools/logger';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import middy from '@middy/core';
 
-const logger = new Logger({ serviceName: 'localstack' });
+const logger = new Logger();
 
-export const handler = async (_event, _context): Promise<void> => {
-  logger.info('Hello World');
+const lambdaHandler = async (
+  _event: unknown,
+  _context: unknown
+): Promise<void> => {
+  logger.info('This is an INFO log with some context');
 };
+
+export const handler = middy(lambdaHandler).use(
+  injectLambdaContext(logger, { logEvent: true })
+);
